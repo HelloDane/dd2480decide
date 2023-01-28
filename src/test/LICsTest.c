6a414a1effbe962e322adb9447f5863ab4e59478
Marcus Friberg <xolvez@gmail.com>
2023-01-28 18:37:40 +0100

    LIC3isMet implementation and testing
    feat. Implemented LIC3isMet and wrote multiple tests to make sure it
    behaves as expected.

--- src/test/LICsTest.c
+++ src/test/LICsTest.c
@@ -26,7 +26,52 @@ void testLIC2isMet() {
  * TODO
  */
 void testLIC3isMet() {
-  
+
+  // In the problem instance below, points 1, 2 and 3 form a triangle with area 2 * 2 / 2 = 2 units
+  // The fourth point lies between them, causing a smaller area when combined with any pair of other points
+  NUMPOINTS = 4;
+  X[0] = 1;
+  Y[0] = 1;
+  X[1] = 2;
+  Y[1] = 3;
+  X[2] = 3;
+  Y[2] = 1;
+  X[3] = 2;
+  Y[3] = 1.8;
+
+  PARAMETERS.AREA1 = 1.95;
+  boolean isMet = LIC3isMet(); // Should be true since 2 > 1.95
+  if(!isMet) {
+    LOGE("LIC3isMet returned false when it should have returned true");
+  }
+
+  PARAMETERS.AREA1 = 3.2;
+  isMet = LIC3isMet(); // Should be false since 2 < 3.2
+  if(isMet) {
+    LOGE("LIC3isMet returned true when it should have returned false");
+  }
+
+  // reorder points so it is not the 3 first ones that form the triangle (swapped 0 and 3)
+  X[0] = 2;
+  Y[0] = 1.8;
+  X[1] = 2;
+  Y[1] = 3;
+  X[2] = 3;
+  Y[2] = 1;
+  X[3] = 1;
+  Y[3] = 1;
+
+  PARAMETERS.AREA1 = 1.95;
+  isMet = LIC3isMet(); // Should be true since 2 > 1.95
+  if(!isMet) {
+    LOGE("LIC3isMet returned false when it should have returned true");
+  }
+
+  PARAMETERS.AREA1 = 3.2;
+  isMet = LIC3isMet(); // Should be false since 2 < 3.2
+  if(isMet) {
+    LOGE("LIC3isMet returned true when it should have returned false");
+  }
 }
 
 /**
